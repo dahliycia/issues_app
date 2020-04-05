7fa3a49bf1ecbf0907bf4d48e3640370f258d11c
dahliycia <nnnnodahlia@gmail.com>
2020-04-05 20:40:12 +0200

    initial server setup

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM   node:lts-alpine
+
+ENV    DATA_DIR='/data'
+
+RUN    mkdir /app
+RUN    chown node:node /app
+RUN    mkdir /data
+RUN    chown node:node /data
+
+USER   node
+WORKDIR /app
+COPY   --chown node:node . ./
+
+RUN    npm config set registry http://registry.npmjs.org/;
+
+RUN    npm install;
+
+EXPOSE ${API_PORT} || 8001
+
+CMD    node server.js;
---
+++ server.js
@@ -0,0 +1,60 @@
+const Hapi = require('@hapi/hapi');
+const Boom = require('@hapi/boom');
+const Pino = require('hapi-pino');
+const Inert = require('@hapi/inert');
+const Vision = require('vision');
+const Swagger = require('hapi-swagger');
+
+const Pack = require('./package');
+const Api = require('./lib');
+
+const server = Hapi.server({
+  port: process.env.API_PORT || 8001,
+  routes: {
+    validate: {
+      failAction: async (request, h, err) => {
+        console.error(err); throw err;
+      }
+    }
+  }
+})
+
+const swaggerOptions = {
+  "documentationPath": "/documentation",
+  "jsonPath": "/swagger.json",
+  "swaggerUIPath": "/swaggerui/",
+  "host": "127.0.0.1:8001",
+  "info": {
+    "title": "Cracker - Issues Tracker API",
+    "version": Pack.version,
+    "description": "API for the Cracker - Issues Tracker"
+  }
+};
+
+(async () => {
+  await Api.initialize(server, {});
+
+  await server.register({
+    plugin: Pino,
+    options: {
+      redact: ['req.headers', 'res.headers']
+    }
+  });
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: Swagger,
+      options: swaggerOptions
+    }
+  ])
+
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+})();
+
